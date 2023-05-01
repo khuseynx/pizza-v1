a16bd8d2a46e9bea24a01c873b08b8e8217c52be
Khuseyn <107132248+khuseynx@users.noreply.github.com>
2023-05-01 23:35:36 +0300

    v2
    New pages, first components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React from "react";
+
+import Header from "./sections/Header";
+import Categories from "./sections/Categories";
+import Sort from "./sections/Sort";
+import PizzaBlock from "./sections/PizzaBlock";
+import "./scss/app.scss";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there</h1>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price={499} />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
