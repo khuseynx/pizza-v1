3194675caa73170f73843d57c267cdbc991c3121
Khuseyn <107132248+khuseynx@users.noreply.github.com>
2023-05-13 10:47:49 +0300

    v9
    Global state and sort

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Categories from "../sections/Categories";
+import Categories from '../sections/Categories';
 import Sort from "../sections/Sort";
 import PizzaBlock from "../sections/PizzaBlock/index";
 import Skeleton from "../sections/PizzaBlock/Skeleton";
@@ -8,23 +8,39 @@ import Skeleton from "../sections/PizzaBlock/Skeleton";
 const Home = () => {
   const [pizzas, setPizzas] = React.useState([]);
   const [pageIsLoading, setPageIsLoading] = React.useState(true);
+  const [categoryIndex, setCategoryIndex] = React.useState(0);
+  const [sortGroup, setSortGroup] = React.useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  });
 
   React.useEffect(() => {
-    fetch("https://6455553bf803f3457640a60f.mockapi.io/data")
+
+  const order = sortGroup.sortProperty.includes('-') ? 'asc' : 'desc';
+  const sortBy = sortGroup.sortProperty.replace('-','');
+  const category = categoryIndex > 0 ? `category=${categoryIndex}` : "";
+  setPageIsLoading(true);
+
+    fetch(
+      `https://6455553bf803f3457640a60f.mockapi.io/data?${category}&sortBy=${sortBy}&order=${order}`)
       .then((res) => res.json())
       .then((array) => {
         setPizzas(array);
         setPageIsLoading(false);
-        console.log("array of my pizzas:", array);
       });
-      window.scrollTo(0, 0);
-  }, []);
+    window.scrollTo(0, 0);
+  }, [categoryIndex, sortGroup]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories
+          value={categoryIndex}
+          OnClickCategory={(i) => setCategoryIndex(i)}
+        />
+        <Sort
+         value={sortGroup} 
+         clickOnSort={(i) => setSortGroup(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
