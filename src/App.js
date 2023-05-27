2941184ed04fd25192ec1cb815cd1f0038877212
Khuseyn <107132248+khuseynx@users.noreply.github.com>
2023-05-27 21:19:19 +0300

    v12
    redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/filterSlice";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./sections/Header";
@@ -12,9 +14,23 @@ export const mySearchContext = React.createContext();
 
 function App() {
   const [valueOfSearch, setValueOfSearch] = React.useState("");
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div className="wrapper">
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      ></button>
+
       <mySearchContext.Provider value={{ valueOfSearch, setValueOfSearch }}>
         <Header />
         <div className="content">
